313466d82b2ab45226aedb1d22d61e0293f5b728
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-06-01 20:55:45 -0500

    feat: Got state set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
+import React, { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import axios from 'axios'
 
 import Home from './components/HomeComponent'
 import Favorites from './components/FavoritesComponent'
 import Single from './components/SinglePokemonComponent'
 
+import { getKanto } from './utilities/utilities'
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const [kantoDex, setKantoDex] =  useState([])
+  
+  const getKanto = async () => {
+    const holder = []
+
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=150')
+        console.log(res.data.results)
+        setKantoDex(res.data.results)
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
+
+  useEffect(() => {
+    getKanto()    
+  }, [])
+  
   return (
     <div className="App">
       <Router>
@@ -17,7 +40,7 @@ function App() {
         </Switch>
       </Router>
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/utilities/utilities.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export const getKanto = async () => {
+    const holder = []
+
+    try {
+        const res = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=150')
+        console.log(res.data.results)
+        return res.data.results
+    } catch (error) {
+        console.log(error)
+        return null
+    }
+}
