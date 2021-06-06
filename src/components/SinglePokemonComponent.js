1b5c2b6bd83bbeafa5c4655133ba05a1404ed472
Daniel Cortes <daniel.cortes.83944@gmail.com>
2021-06-06 18:29:49 -0500

    feat: Table styling

--- src/components/SinglePokemonComponent.js
+++ src/components/SinglePokemonComponent.js
@@ -51,90 +51,82 @@ const SinglePokemonComponent = (props) => {
     if(singlePokemon){
         return (
             <div className="container">
-                <div className="dex-body row d-flex justify-content-between">
+                <div className="dex-body ">
+                    <div className="row d-flex justify-content-between">
+                        {/* READOUT */}
+                        <div className="col-12 col-md-5 info-body d-flex flex-column justify-content-between p-2">
 
-                    {/* READOUT */}
-                    <div className="col-5 info-body d-flex flex-column justify-content-between p-2">
+                            <div className="d-flex flex-column flex-md-row align-items-center justify-content-between">
+                                <h2 className="no-marg">{props.match.params.pokemon}</h2>
+                                <p className="no-marg">Height: {height}'</p>
+                            </div>
 
-                        <div className="d-flex flex-column flex-md-row align-items-center justify-content-between">
-                            <h2 className="no-marg">{props.match.params.pokemon}</h2>
-                            <p className="no-marg">Height: {height}'</p>
+                            <div>
+                                <span>Types:</span>
+                                <div className="row">
+                                    {types.map((type, index) => {
+                                        return (
+                                            <span key={index} className="col-12 col-md-6 text-center">
+                                                <div className={type.type.name}>{type.type.name}</div>
+                                            </span>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+
+                            <div>
+                                <span>Abilities:</span>
+                                <div className="row">
+                                    {abilities.map((ability, index) => {
+                                        return (
+                                            <span key={index} className="col-12 col-md-6 text-center">
+                                                <div className="dark">{ability.ability.name}</div>
+                                            </span>
+                                        )
+                                    })}
+                                </div>
+                            </div> 
+
+                            {isFavorite &&
+                                <div className="text-center">
+                                    <span>This is one of your favorites.</span>
+                                    <button className="butn" id="btn2" onClick={() => toggleFavorite(singlePokemon)}>Remove?</button>
+                                </div>
+                            }    
+                            {!isFavorite &&
+                                <div className="text-center">
+                                    <button className="butn" id="btn2" onClick={() => toggleFavorite(singlePokemon)}>Add To Favorites?</button>
+                                </div>
+                            } 
+
                         </div>
 
-                        <div>
-                            <span>Types:</span>
-                            <div className="row">
-                                {types.map((type, index) => {
+                        {/* IMAGE */}
+                        <div className="img-wrapper col-12 col-md-5">
+                            <div className="img-field">
+                                <img src={pokeSprite} alt="Sprite of Pokemon"/>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div className="stats mt-3">
+                        <table >
+                            <tr className="row text-center">
+                                {baseStats.map(stat => {
                                     return (
-                                        <span key={index} className="col-12 col-md-6 text-center p-1">
-                                            <div className={type.type.name}>{type.type.name}</div>
-                                        </span>
+                                        <th className="col-2">{stat.stat.name}</th>
                                     )
                                 })}
-                            </div>
-                        </div>
-
-                        <div>
-                            <span>Abilities:</span>
-                            <div className="row">
-                                {abilities.map((ability, index) => {
+                            </tr>
+                            <tr className="row text-center">
+                                {baseStats.map(stat => {
                                     return (
-                                        <span key={index} className="col-12 col-md-6 text-center p-1">
-                                            <div className="black">{ability.ability.name}</div>
-                                        </span>
+                                        <th className="col-2">{stat.base_stat}</th>
                                     )
                                 })}
-                            </div>
-                        </div> 
-
-                        {isFavorite &&
-                            <div className="text-center">
-                                <span>This is one of your favorites.</span>
-                                <button onClick={() => toggleFavorite(singlePokemon)}>Remove?</button>
-                            </div>
-                        }    
-                        {!isFavorite &&
-                            <div className="text-center">
-                                <button onClick={() => toggleFavorite(singlePokemon)}>Add To Favorites?</button>
-                            </div>
-                        } 
-
+                            </tr>
+                        </table>
                     </div>
-
-                    {/* IMAGE */}
-                    <div className="img-wrapper col-5">
-                        <div className="img-field">
-                            <img src={pokeSprite} alt="Sprite of Pokemon"/>
-                        </div>
-                    </div>
-                
-                <div>
-
-                <div>
-                    <table>
-                        <tr>
-                            {baseStats.map(stat => {
-                                return (
-                                    <th>{stat.stat.name}</th>
-                                )
-                            })}
-                        </tr>
-                        <tr>
-                            {baseStats.map(stat => {
-                                return (
-                                    <th>{stat.base_stat}</th>
-                                )
-                            })}
-                        </tr>
-                    </table>
-                </div>
-
-
-            
-                </div>
-            
-                
-            
                 </div>
             </div>
         )
@@ -150,3 +142,4 @@ const SinglePokemonComponent = (props) => {
 
 
 export default withRouter(SinglePokemonComponent)
+
